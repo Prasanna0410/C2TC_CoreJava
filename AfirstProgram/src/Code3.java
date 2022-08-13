0ff86e2120a532aae728181c2bb10154ae864109
Prasa <Prasa@DESKTOP-KI556SJ>
2022-08-13 14:18:55 +0530

    first commit

---
+++ AfirstProgram/src/Code3.java
@@ -0,0 +1,14 @@
+
+public class Code3 {
+
+	public static void main(String[] args) {
+		int numA=5;
+		int numB=10;
+		int numC=0;
+		
+		int numD= --numC + numB--; 
+		
+		System.out.println(numD);
+	}
+
+}
